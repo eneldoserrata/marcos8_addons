2634c5902850ac6ae68dc0364a28f230562abf05
Eneldo Serrata <eneldoserrata@gmail.com>
2016-04-14 02:07:59 -0400

    ncf now can install with previews data

---
+++ visanet_colors/__openerp__.py
@@ -0,0 +1,17 @@
+{
+    'name' : 'Custom menu bar (orange)',
+    'version' : '1.0.0',
+    'author' : 'Ivan Yelizariev',
+    'category' : 'Base',
+    'website' : 'https://yelizariev.github.io',
+    'description': """
+
+    """,
+    'depends' : ['base'],
+    'data':[
+        'views.xml',
+        # 'data/data.xml'
+
+        ],
+    'installable': True
+}
