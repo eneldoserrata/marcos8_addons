8dd45806cdf92879b93127903b9341962ba5a946
Eneldo Serrata <eneldoserrata@gmail.com>
2016-08-09 03:00:19 -0400

    VALIDAR LAS FECHAS DE FINAL DEL PERIODO DE PRUEBA QUE NO SOBREPASE LOS TRES MESE

--- l10n_do_hr_payroll/models/contract.py
+++ l10n_do_hr_payroll/models/contract.py
@@ -12,9 +12,18 @@ class HrContract(models.Model):
 
     log_ids = fields.One2many("salary.log", "contract_id", string="Historial de salarios")
     state = fields.Selection([("draft","Borrador"),
-                               ("active","Activo"),
+                               ("trial",u"Período de prueba"),
+                               ("active", "Activo"),
                                ("req_liquidated",u"Solicitud de Liquidación"),
                                ("liquidated", "Liquidado")], default="draft", string="Estado")
+
+    @api.onchange("trial_date_end")
+    def onchacnge_trial_date_end(self):
+        d2 = datetime.strptime(self.trial_date_start, "%Y-%m-%d").date()
+        d1 = datetime.strptime(self.trial_date_end, "%Y-%m-%d").date()
+        month = (d1.year - d2.year) * 12 + d1.month - d2.month
+        if month > 3:
+            raise exceptions.ValidationError("La fecha final del periodo de prueba no debe sobrepasar los 3 meses.")
 
     @api.onchange("trial_date_start")
     def onchange_trial_date_start(self):
@@ -23,6 +32,10 @@ class HrContract(models.Model):
         date_start = (trial_date_end+timedelta(days=1))
         self.trial_date_end = trial_date_end.isoformat()
         self.date_start = date_start.isoformat()
+
+    @api.multi
+    def set_trial(self):
+        self.state = "trial"
 
     @api.multi
     def set_active(self):
