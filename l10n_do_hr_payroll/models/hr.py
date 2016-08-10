da26bd24bdc5f1e9e8f91a26583c847052a93ebd
Eneldo Serrata <eneldoserrata@gmail.com>
2016-08-10 00:53:12 -0400

    IMP

--- l10n_do_hr_payroll/models/hr.py
+++ l10n_do_hr_payroll/models/hr.py
@@ -28,19 +28,34 @@ class NewModule(models.Model):
     def _get_nomina(self):
         selecction = []
         for i in range(1,20):
-            selecction.append((1, "Nomina {}".format(i)))
+            selecction.append((str(i), u"Nómina {}".format(i)))
 
         return selecction
 
     NUMERO_NOMINA = fields.Selection(_get_nomina, string=u"ID Nómina", default=1, required=True)
     TIPO_DE_DOCUMENTO = fields.Selection([('C',u'Cédula'),('N','NSS'),('P','Pasaporte')], required=True, string=u"Tipo de Documento", default="C")
     NUMERO_DE_DOCUMENTO = fields.Char(string=u"Número de documento", help="Indique el numero del tipo de documento.")
+    PRIMER_APELLIDO = fields.Char("Primer Apellido")
+    SEGUNDO_APELLIDO = fields.Char("Segundo Apellido")
+
 
 
 class HrSalaryRule(models.Model):
     _inherit = "hr.salary.rule"
 
     rule_const = fields.Boolean(help="This si tru only for contant values rules")
+
+
+class HrPayslip(models.Model):
+    _inherit = "hr.payslip"
+
+    TIPO_INGRESO = fields.Selection([('0001', u'Normal'),
+                                            ('0002', u'Trabajador Ocasional (no fijo'),
+                                            ('0003', u'Asalariado por hora o labora tiempo parcial'),
+                                            ('0004',u'No laboro mes completo por razones varias'),
+                                            ('0005',u'Salario prorrateado semanal/bisemanal'),
+                                            ('0006',u'Pensionado antes de la Ley 87-01'),
+                                            ('0007',u'Exento por Ley de pago al SDSS')], string=u"Tipo de remuneración", default="0001", required=True)
 
 
 
