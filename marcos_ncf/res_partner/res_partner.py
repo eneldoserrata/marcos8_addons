6789c454f421b99165ba2a59fa59746dbc01021f
Eneldo Serrata <eneldoserrata@gmail.com>
2016-08-24 02:00:12 -0400

    partner IMP

--- marcos_ncf/res_partner/res_partner.py
+++ marcos_ncf/res_partner/res_partner.py
@@ -1,242 +1,178 @@
 # -*- coding: utf-8 -*-
-##############################################################################
+########################################################################################################################
+#  Copyright (c) 2015 - Marcos Organizador de Negocios SRL. (<https://marcos.do/>) #  Write by Eneldo Serrata (eneldo@marcos.do)
+#  See LICENSE file for full copyright and licensing details.
 #
-#    Copyright (C) 2013-2015 Marcos Organizador de Negocios SRL http://marcos.do
-#    Write by Eneldo Serrata (eneldo@marcos.do)
+# Odoo Proprietary License v1.0
 #
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
+# This software and associated files (the "Software") may only be used
+# (nobody can redistribute (or sell) your module once they have bought it, unless you gave them your consent)
+# if you have purchased a valid license
+# from the authors, typically via Odoo Apps, or if you have received a written
+# agreement from the authors of the Software (see the COPYRIGHT file).
 #
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
+# You may develop Odoo modules that use the Software as a library (typically
+# by depending on it, importing it and using its resources), but without copying
+# any source code or material from the Software. You may distribute those
+# modules under the license of your choice, provided that this license is
+# compatible with the terms of the Odoo Proprietary License (For example:
+# LGPL, MIT, or proprietary licenses similar to this one).
 #
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# It is forbidden to publish, distribute, sublicense, or sell copies of the Software
+# or modified copies of the Software.
 #
-##############################################################################
-
+# The above copyright notice and this permission notice must be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+########################################################################################################################
 from ..tools import is_identification, _internet_on
-from openerp.osv import osv, fields
 import requests
-from openerp import api, exceptions
-
+from openerp import models, api, exceptions, fields
 import re
 
-class res_partner(osv.Model):
-    _name = "res.partner"
+import logging
+
+
+_logger = logging.getLogger(__name__)
+
+
+class ResPartner(models.Model):
     _inherit = "res.partner"
 
-    def _check_unique_ref(self, cr, uid, ids, context=None):
-        partner = self.browse(cr, uid, ids, context=context)[0]
-        if partner.customer or partner.supplier:
-            if not self.search(cr, uid, [("ref", "=", partner.ref), ("multiple_company_rnc", "=", False)]):
-                return True
+    invoice_method = fields.Selection([('manual', 'Se digitaran las facturas manualmente'),
+                                        ('order', 'El proveedor envia factura definitiva'),
+                                        (
+                                            'picking',
+                                            'El proveedor envia orden de entrega y luego envia la factura')],
+                                       'Invoicing Control',
+                                       help=u"""
+                                                    Sobre la base de las líneas de orden de compra: Lugar de líneas\n
+                                                    individuales en 'Factura De control> Basado en p.o. líneas "desde\n
+                                                    donde se puede selectiva crear y factura. Sobre la base de la factura\n
+                                                    generada: crear un proyecto de facturar puede validar más tarde. Bases\n
+                                                    de los envíos entrantes: permiten crear una factura cuando se validan\n
+                                                    recepciones.
+                                                    """
+                                       )
+    # multiple_company_rnc = fields.Boolean(u"RNC para varias compañias",
+    #                                        help=u"Esto permite poder utilizar el RNC en varios registros de compañias"),
+    ref_type = fields.Selection([("cedula", u"Cédula"),
+                                  ("rnc", u"RNC"),
+                                  ("pasport", u"Pasaporte"),
+                                  ("none", u"Otros"),
+                                  ], string=u"Tipo de identificación", required=True, default="cedula")
+
+    @api.model
+    def name_search(self, name, args=None, operator='ilike', limit=100):
+        res = super(ResPartner, self).name_search(name, args=args, operator=operator, limit=100)
+        if not res and name:
+            if len(name) in (9,11):
+                partners = self.search([('ref','=',name)])
+            else:
+                partners = self.search([('ref','ilike',name)])
+
+            if partners:
+                res = partners.name_get()
+        return res
+
+    @api.model
+    def ref_is_unique(self, fiscal_id):
+        partner = self.search([('ref', '=', fiscal_id)])
+        if partner:
+            raise exceptions.UserError(u"Ya fue registrada una empresa con este numero de RNC/Cédula")
         return False
 
-    _columns = {
-        'invoice_method': fields.selection([('manual', 'Se digitaran las facturas manualmente'),
-                                            ('order', 'El proveedor envia factura definitiva'),
-                                            (
-                                                'picking',
-                                                'El proveedor envia orden de entrega y luego envia la factura')],
-                                           'Invoicing Control',
-                                           help=u"""
-                                                Sobre la base de las líneas de orden de compra: Lugar de líneas\n
-                                                individuales en 'Factura De control> Basado en p.o. líneas "desde\n
-                                                donde se puede selectiva crear y factura. Sobre la base de la factura\n
-                                                generada: crear un proyecto de facturar puede validar más tarde. Bases\n
-                                                de los envíos entrantes: permiten crear una factura cuando se validan\n
-                                                recepciones.
-                                                """
-                                           ),
-        'multiple_company_rnc': fields.boolean(u"RNC para varias compañias",
-                                               help=u"Esto permite poder utilizar el RNC en varios registros de compañias"),
-        'ref_type': fields.selection([("cedula",u"Cédula"),
-                                     ("rnc", u"RNC"),
-                                     ("pasport", u"Pasaporte"),
-                                     ("none", u"Otro"),
-                                     ], string=u"Tipo de identificación", required=True, default="cedula")
-
-
-    }
-
-    _constraints = [
-        (osv.osv._check_recursion, 'You cannot create recursive Partner hierarchies.', ['parent_id']),
-        (_check_unique_ref,
-         u"Esta identificacion ya ha sido registrado! Si quiere utilizar varios relacionados con mismo RNC/Cedula debe indicarlo en el campo --RNC para varias compañias--",
-         [u"Rnc/Cédula"]),
-    ]
-
-    def name_search(self, cr, uid, name, args=None, operator='ilike', context=None, limit=100):
-        if not args:
-            args = []
-        if name and operator in ('=', 'ilike', '=ilike', 'like', '=like'):
-
-            self.check_access_rights(cr, uid, 'read')
-            where_query = self._where_calc(cr, uid, args, context=context)
-            self._apply_ir_rules(cr, uid, where_query, 'read', context=context)
-            from_clause, where_clause, where_clause_params = where_query.get_sql()
-            where_str = where_clause and (" WHERE %s AND " % where_clause) or ' WHERE '
-
-            # search on the name of the contacts and of its company
-            search_name = name
-            if operator in ('ilike', 'like'):
-                search_name = '%%%s%%' % name
-            if operator in ('=ilike', '=like'):
-                operator = operator[1:]
-
-            # TODO: simplify this in trunk with `display_name`, once it is stored
-            # Perf note: a CTE expression (WITH ...) seems to have an even higher cost
-            #            than this query with duplicated CASE expressions. The bulk of
-            #            the cost is the ORDER BY, and it is inevitable if we want
-            #            relevant results for the next step, otherwise we'd return
-            #            a random selection of `limit` results.
-            query = ('''SELECT res_partner.id FROM res_partner
-                                  LEFT JOIN res_partner company
-                                       ON res_partner.parent_id = company.id'''
-                     + where_str + ''' (res_partner.email ''' + operator + ''' %s OR  res_partner.ref ''' + operator + ''' %s OR
-                      CASE
-                           WHEN company.id IS NULL OR res_partner.is_company
-                               THEN res_partner.name
-                           ELSE company.name || ', ' || res_partner.name
-                      END ''' + operator + ''' %s)
-                ORDER BY
-                      CASE
-                           WHEN company.id IS NULL OR res_partner.is_company
-                               THEN res_partner.name
-                           ELSE company.name || ', ' || res_partner.name
-                      END''')
-
-            where_clause_params += [search_name, search_name]
-            where_clause_params.append(search_name)
-            if limit:
-                query += ' limit %s'
-                where_clause_params.append(limit)
-
-            cr.execute(query, where_clause_params)
-            ids = map(lambda x: x[0], cr.fetchall())
-
-            if ids:
-                return self.name_get(cr, uid, ids, context)
-            else:
-                return []
-        return super(res_partner, self).name_search(cr, uid, name, args, operator=operator, context=context,
-                                                    limit=limit)
-
-    def get_rnc(self, ref):
-        res = requests.get('http://api.marcos.do/rnc/%s' % ref)
+    def get_rnc(self, fiscal_id):
+        res = requests.get('http://api.marcos.do/rnc/%s' % fiscal_id)
         if res.status_code == 200:
             return res.json()
         else:
             return False
 
-    def is_fiscal_id(self, vals):
-        value = vals.get("ref", False) or vals.get("name", False)
-        value = re.sub("[^0-9]", "", value.strip())
-
-        if value and (len(value) == 9 or len(value) == 11):
-            if value.isdigit():
-                return value
-
-        return False
-
-    def create(self, cr, uid, vals, context=None):
-
-        if vals.get("fiscal_position", False):
-            fiscal_id = self.pool.get("account.fiscal.position").search(cr, uid, [("fiscal_type", "=", vals["fiscal_position"])])
-            if fiscal_id:
-                vals.update({"property_account_position": fiscal_id[0]})
-
-        validation = {}
-
-        fiscal_id = self.is_fiscal_id(vals)
-        if fiscal_id:
-            validation = self.validate_fiscal_id(fiscal_id, context=context)
-
-        if validation:
-            if vals.get("property_account_position", False):
-                validation.update({"property_account_position": vals["property_account_position"]})
-            vals.update(validation)
-
-
-        try:
-            name_is_numeric = vals["name"].isnumeric()
-        except:
-            name_is_numeric = unicode(vals["name"], 'utf-8').isnumeric()
-
-
-        if name_is_numeric:
-            raise exceptions.ValidationError(u"El número de cédula o rnc no es valido!")
-        return super(res_partner, self).create(cr, uid, vals, context=context)
-
-    def write(self, cr, uid, ids, vals, context=None):
-        partner = self.browse(cr, uid, ids)
-
-        if vals.get("fiscal_position", False):
-            fiscal_id = self.pool.get("account.fiscal.position").search(cr, uid, [("fiscal_type", "=", vals["fiscal_position"])])
-            if fiscal_id:
-                vals.update({"property_account_position": fiscal_id[0]})
-
-        validation = {}
-        fiscal_id = self.is_fiscal_id(vals)
-
-        if fiscal_id:
-            if vals.get("ref", False):
-                validation = self.validate_fiscal_id(u"{}".format(vals["ref"]), context=context)
-            elif vals.get("name", False):
-                validation = self.validate_fiscal_id(u"{}".format(vals["name"]), context=context)
-
-        if validation:
-            if vals.get("property_account_position", False):
-                validation.update({"property_account_position": vals["property_account_position"]})
-            vals.update(validation)
-
-        return super(res_partner, self).write(cr, uid, ids, vals, context=context)
-
-    def validate_fiscal_id(self, name, context=None):
+    def validate_fiscal_id(self, fiscal_id):
         vals = {}
-        context = context or {}
-        supplier = customer = False
 
-        if context.get('search_default_supplier', False):
-            supplier = True
+        if not len(fiscal_id) in [9, 11]:
+            raise exceptions.ValidationError(u"Debe colocar un numero de RNC/Cedula valido!")
         else:
-            customer = True
-        if name and not len(name) in [9, 11] and name.isnumeric():
-            raise osv.except_osv(u"Debe colocar un numero de RNC/Cedula valido!", u"RNC/Cedula")
-
-        if name.isdigit() and len(name) in [9, 11]:
             if _internet_on():
-                data = self.get_rnc(name)
+                data = self.get_rnc(fiscal_id)
                 if data:
                     vals['ref'] = data['rnc'].strip()
                     vals['name'] = data['name'].strip()
-                    vals["comment"] = u"Nombre Comercial: %s, regimen de pago: %s,  estatus: %s, categoria: %s" % (
+                    vals["comment"] = u"Nombre Comercial: {}, regimen de pago: {},  estatus: {}, categoria: {}".format(
                         data['comercial_name'], data['payment_regimen'], data['status'], data['category'])
-                    vals['is_company'] = True
-
-                    if customer:
-                        vals['property_account_position'] = 1
-                    elif supplier:
-                        vals["property_account_position"] = 13
-            else:
-                vals['ref'] = name.strip()
-                vals['is_company'] = True
-
+                    vals.update({"company_type": "company"})
+                    if len(fiscal_id) == 9:
+                        vals.update({"company_type": "company"})
+                    else:
+                        vals.update({"company_type": "person"})
         return vals
 
     @api.model
-    def name_create(self, name):
-        vals = self.validate_fiscal_id(name, context=self._context)
-        if self._rec_name:
-            if vals:
-                record = self.create(vals)
-                return record.name_get()[0]
+    def check_vals(self, vals):
+        if self._context.get("install_mode", False):
+            return super(ResPartner, self).check_vals(vals)
+        if isinstance(vals, unicode):
+            vals = {"ref": vals}
+
+        if vals:
+            ref_or_name = vals.get("ref", False) or vals.get("name", False)
+
+            ref_type = vals.get("ref_type", False) or self.ref_type
+            ref_or_name = re.sub("[^0-9]", "", ref_or_name.strip())
+
+            if ref_type == "cedula" and not len(ref_or_name) == 11:
+                raise exceptions.ValidationError(u"Una cédula valida solo debe tener 11 digitos.")
+            if ref_type == "rnc" and not len(ref_or_name) == 9:
+                raise exceptions.ValidationError(u"Un RNC valido solo debe tener 9 digitos.")
+
+            if ref_type in ("pasport", "none"):
+                return vals
+
+            if ref_or_name:
+                if ref_or_name.isdigit():
+                    fiscal_id = ref_or_name.strip()
+                    vals = self.validate_fiscal_id(fiscal_id)
+        return vals
+
+    @api.model
+    def create(self, vals):
+        if self._context.get("install_mode", False):
+            return super(ResPartner, self).create(vals)
+        elif vals:
+            validation = self.check_vals(vals)
+            if validation:
+                vals.update(validation)
             else:
-                raise exceptions.ValidationError("A session's instructor can't be an attendee")
+                raise exceptions.UserError(u"El número de RNC/Cédula no es vEalido en la DGII.")
+            return super(ResPartner, self).create(vals)
+
+    @api.onchange("ref")
+    def onchange_ref(self):
+        if self.ref and not self._context.get("install_mode", False):
+            if self.env["account.invoice"].search_count([('partner_id', '=', self.id)]):
+                raise exceptions.UserError("No puede cambiar el RNC/Cédula ya que este cliente o proveedor tiene facturas.")
+            self.check_vals(self.ref)
+
+    @api.model
+    def name_create(self, name):
+        if self._context.get("install_mode", False):
+            return super(ResPartner, self).name_create(name)
+        if self._rec_name:
+            if name.isdigit():
+                partner = self.search([('ref', '=', name)])
+                if partner or not len(name) in [9, 11] or not self.get_rnc(name):
+                    return (0,"")
+            record = self.create({self._rec_name: name})
+            return record.name_get()[0]
         else:
-            return super(res_partner, self).name_create(name)
+            _logger.warning("Cannot execute name_create, no _rec_name defined on %s", self._name)
+            return False
